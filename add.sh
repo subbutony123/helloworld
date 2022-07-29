0fa05aa1c9dcae8d5a39e3c365f4e9542ffe7fe7
Ubuntu <ubuntu@ip-10-0-0-207.ap-south-1.compute.internal>
2022-07-29 16:07:55 +0000

    first commit

---
+++ add.sh
@@ -0,0 +1,7 @@
+echo -n "Enter first number"
+read first_number
+echo -n "Enter second number"
+read second_number
+Sum=$(($first_number +$second_number))
+echo "Sum of $first_number and $second_number: $Sum"
+
